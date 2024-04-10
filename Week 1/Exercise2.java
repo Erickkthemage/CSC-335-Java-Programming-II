3b36a6c1e66efb9142180b7217bea2880b1113c1
Erickkthemage <155121981+Erickkthemage@users.noreply.github.com>
2024-04-10 16:45:10 -0400

    Create Exercise2.java

---
+++ Week 1/Exercise2.java
@@ -0,0 +1,26 @@
+//add class definitions below this line
+class Cat {
+  String breed;
+  String color;
+  String name;
+
+  Cat() {
+    breed = "american shorthair";
+    color = "black";
+    name = "kiwi";
+  }
+}
+    
+
+  
+//add class definitions above this line
+
+public class Exercise2 {
+  public static void main(String[] args) {
+    
+    //add code below this line
+    
+
+    //add code above this line
+  }
+}
