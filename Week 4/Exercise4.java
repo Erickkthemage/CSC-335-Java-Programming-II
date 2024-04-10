3d863d1d9d0f0c3d3c1a66f1bc3df382f232a015
Erickkthemage <155121981+Erickkthemage@users.noreply.github.com>
2024-04-10 17:40:13 -0400

    Create Exercise4.java

---
+++ Week 4/Exercise4.java
@@ -0,0 +1,78 @@
+class Bank {
+  private String name;
+  private int customers;
+  private double[] accounts;
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String newName) {
+    name = newName;
+  }
+  
+  public int getCustomers() {
+    return customers;
+  }
+  
+  public void setCustomers(int newCustomers) {
+    customers = newCustomers;
+  }
+  
+  public double[] getAccounts() {
+    return accounts;
+  }
+  
+  public void setAccounts(double[] newAccounts) {
+    accounts = newAccounts;
+  }
+  
+  public double branchTotal() {
+    double total = 0;
+    for (double account : accounts) {
+      total += account;
+    }
+    return total;
+  }
+}
+
+//add class definitions below this line
+
+class RegionalBank extends Bank {
+  private double[][] regionalAccounts;
+
+  public RegionalBank(String n, int c, double[][] ra) {
+    setName(n);
+    setCustomers(c);
+    regionalAccounts = ra;
+  }
+
+  public double regionalTotal() {
+    double total = 0;
+    for (double[] regionalAccountRow : regionalAccounts) {
+      for (double regionalAccount : regionalAccountRow) {
+        total += regionalAccount;
+      }
+    }
+    return total;
+  }
+}
+
+
+
+//add class definitions above this line
+
+public class Exercise4 {
+  public static void main(String[] args) {
+    
+    //add code below this line
+
+    double[][] accounts = {{10000, 13000, 22000},
+                           {30000, 7000, 19000},
+                           {15000, 23000, 31000}};
+    RegionalBank rb = new RegionalBank("Main Street Bank", 9, accounts);
+    System.out.println(rb.regionalTotal());
+  
+    //add code above this line  
+  }
+}
