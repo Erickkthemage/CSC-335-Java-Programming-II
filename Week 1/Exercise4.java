cbab423c92da8474c1e204fe07d8e887470920a1
Erickkthemage <155121981+Erickkthemage@users.noreply.github.com>
2024-04-10 16:57:43 -0400

    Create Exercise4.java

---
+++ Week 1/Exercise4.java
@@ -0,0 +1,31 @@
+//add class definitions below this line
+class Observation {
+  String date;
+  double temperature;
+  double elevation;
+  int penguins;
+  double precipitation;
+
+  Observation(String d, double t, double e, int p) {
+    date = d;
+    temperature = t;
+    elevation = e;
+    penguins = p;
+    precipitation = 0;
+  }
+}
+    
+
+  
+//add class definitions above this line
+
+public class Exercise4 {
+  public static void main(String[] args) {
+    
+    //add code below this line
+
+    
+
+    //add code above this line
+  }
+}
