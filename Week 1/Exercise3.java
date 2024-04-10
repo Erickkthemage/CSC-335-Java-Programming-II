fa50ce44caff13e567d1f757ec3ab434dfefd809
Erickkthemage <155121981+Erickkthemage@users.noreply.github.com>
2024-04-10 16:57:15 -0400

    Create Exercise3.java

---
+++ Week 1/Exercise3.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+//add class definitions below this line
+class Superhero {
+  String name;
+  String secretIdentity;
+  String[] powers;
+
+  Superhero(String n, String s, String[] p) {
+    name = n;
+    secretIdentity = s;
+    powers = p;
+  }
+}
+    
+
+  
+//add class definitions above this line
+
+public class Exercise3 {
+  public static void main(String[] args) {
+    
+    //add code below this line
+
+
+
+    //add code above this line
+  }
+}
