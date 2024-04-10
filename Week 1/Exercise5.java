663fb5d1d3f209a21a35d5f76baf93924bb351e7
Erickkthemage <155121981+Erickkthemage@users.noreply.github.com>
2024-04-10 16:58:41 -0400

    Create Exercise5.java

---
+++ Week 1/Exercise5.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+//add class definitions below this line
+class BigCat {
+  static String genus = "panthera";
+  String species;
+  String commonName;
+  String[] habitat;
+
+  BigCat(String s, String c, String[] h) {
+    species = s;
+    commonName = c;
+    habitat = h;
+  }
+}
+    
+
+  
+//add class definitions above this line
+
+public class Exercise5 {
+  public static void main(String[] args) {
+    
+    //add code below this line
+
+    
+
+    //add code above this line
+  }
+}
