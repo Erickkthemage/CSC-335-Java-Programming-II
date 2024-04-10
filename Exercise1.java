cf6b5129bf55f2736467686e7b5d9c433fb8d10f
Erickkthemage <155121981+Erickkthemage@users.noreply.github.com>
2024-04-10 16:43:24 -0400

    Create Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,19 @@
+//add class definitions below this line
+class PracticeClass {
+  String date;
+}
+    
+
+  
+//add class definitions above this line
+
+public class Exercise1 {
+  public static void main(String[] args) {
+    
+    //add code below this line
+
+    
+
+    //add code above this line
+  }
+}
